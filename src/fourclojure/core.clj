0055f028a43ee4ecc9fe1d8e74024d0754937022
Tom George <tg82490@gmail.com>
2013-09-17 17:39:44 -0700

    Added stuff

---
+++ src/fourclojure/core.clj
@@ -0,0 +1,11 @@
+(ns fourclojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!")
+  (println x "fourclojure.core is the best"))
+
+(defn -main
+  []
+  (foo "x"))
